b2696fe15e625755ce98192f7705d2c52fffdc6f
shola-123-hub <vogueleisureservices@gmail.com>
2025-12-30 16:43:58 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Sky College App Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Keeps the app functional
+});
